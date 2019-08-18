530efab85917fb2f0d4a7d652723c46f5a0d6523
jordiguirao92 <38755692+jordiguirao92@users.noreply.github.com>
2019-08-18 11:34:40 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,42 +3,6 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
-
-
 //Selecting studentList.
 const studentList = document.querySelectorAll('.student-item');
 const studentsPerPage = 10;
@@ -92,7 +56,7 @@ if (list.length === 0) {
    console.log('No result');
 } else { 
    //Set the page with students. 
-   for (let i = startIndex; i <= endIndex; i++) {
+   for (let i = startIndex; i <= endIndex && i < list.length; i++) {
       list[i].style.display = 'block';
    }
    noresultDiv.style.display = 'none';
@@ -114,7 +78,7 @@ function appendPageLink (list) {
    displayPage(list, selNumberPage);
 
    //Loop to create the correct number of pages.
-   for (let i = 0; i <= numberPages; i++) {
+   for (let i = 0; i < numberPages; i++) {
       let pageNumberLi = document.createElement('li');
       let pageNumberA = document.createElement('a');
       pageNumberA.href = '#';
